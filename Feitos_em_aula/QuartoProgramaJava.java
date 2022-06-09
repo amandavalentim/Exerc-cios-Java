80043a6db393c8c1d33618d61dde2124235f00b6
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2022-06-08 22:58:30 -0300

    Add files via upload

---
+++ Feitos_em_aula/QuartoProgramaJava.java
@@ -0,0 +1,12 @@
+package javaBasico;
+
+public class QuartoProgramaJava {
+	
+	public static void main(String args[]) {
+		int numero=10;
+		int numero2=30;
+		int soma=numero+numero2;
+		System.out.print("A soma dos valores é: "+soma);
+	}
+
+}
---
+++ Feitos_em_aula/TerceiroProgramaJava.java
@@ -0,0 +1,10 @@
+package javaBasico;
+
+public class TerceiroProgramaJava {
+	
+	public static void main(String args[]) {
+		
+		int numero=10;
+		System.out.print("O valor da variável numero é: "+numero);
+	}
+}
